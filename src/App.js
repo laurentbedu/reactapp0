61daa8ef4d3a8869366fc6215d5311ebf6c034e0
laurentbedu <bedulaurent@gmail.com>
2022-02-01 10:48:56 +0100

    homePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import {Test} from './components/Test';
+import {GenderMenu} from './components/GenderMenu';
 
 function App() {
   return (
     <div className="App">
-      <Test/>
-      <Test/>
+    
+      {/* <GenderMenu/> */}
       
     </div>
   );
